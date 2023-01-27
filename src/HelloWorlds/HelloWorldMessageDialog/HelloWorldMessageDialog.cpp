c6e3e3506bdf2a6a19ad0b31fce94ff79a1ee586
Miklós Márton <martonmiklosqdev@gmail.com>
2023-01-27 19:39:37 +0100

    Test SetIcons too

--- src/HelloWorlds/HelloWorldMessageDialog/HelloWorldMessageDialog.cpp
+++ src/HelloWorlds/HelloWorldMessageDialog/HelloWorldMessageDialog.cpp
@@ -2,20 +2,31 @@
 
 #include "a.xpm"
 #include "b.xpm"
+#include "a16.xpm"
+#include "b32.xpm"
 
 class Application : public wxApp {
   bool OnInit() override {
     auto frame1 = new wxFrame(nullptr, wxID_ANY, "Hello World (message box)");
-    auto icon = wxICON(a);
-    frame1->SetIcon(icon);
+
+    wxIconBundle icon_bundle;
+
+    icon_bundle.AddIcon( wxICON(a) );
+    icon_bundle.AddIcon( wxICON(a16) );
+
+    frame1->SetIcons(icon_bundle);
     
     auto panel1 = new wxPanel(frame1, wxID_ANY);
     
     auto button1 = new wxButton(panel1, wxID_ANY, "Click me", {10, 10});
     button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
         auto frame2 = new wxFrame(nullptr, wxID_ANY, "Hello World (message box)");
-        auto icon = wxICON(b);
-        frame2->SetIcon(icon);
+
+        wxIconBundle icon_bundle;
+
+        icon_bundle.AddIcon( wxICON(b) );
+        icon_bundle.AddIcon( wxICON(b32) );
+        frame2->SetIcons(icon_bundle);
         frame2->Show();
     });
     
