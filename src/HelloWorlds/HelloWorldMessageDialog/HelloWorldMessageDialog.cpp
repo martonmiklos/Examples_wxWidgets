be778d6cd902646f55f88181c2647a4988df3899
Miklós Márton <martonmiklosqdev@gmail.com>
2023-01-27 19:28:47 +0100

    Add dual icon example for Kicad #13670 debugging

--- src/HelloWorlds/HelloWorldMessageDialog/HelloWorldMessageDialog.cpp
+++ src/HelloWorlds/HelloWorldMessageDialog/HelloWorldMessageDialog.cpp
@@ -1,14 +1,22 @@
 #include <wx/wx.h>
 
+#include "a.xpm"
+#include "b.xpm"
+
 class Application : public wxApp {
   bool OnInit() override {
     auto frame1 = new wxFrame(nullptr, wxID_ANY, "Hello World (message box)");
+    auto icon = wxICON(a);
+    frame1->SetIcon(icon);
     
     auto panel1 = new wxPanel(frame1, wxID_ANY);
     
     auto button1 = new wxButton(panel1, wxID_ANY, "Click me", {10, 10});
     button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
-      wxMessageDialog(nullptr, "Hello, World!").ShowModal();
+        auto frame2 = new wxFrame(nullptr, wxID_ANY, "Hello World (message box)");
+        auto icon = wxICON(b);
+        frame2->SetIcon(icon);
+        frame2->Show();
     });
     
     frame1->Show();
